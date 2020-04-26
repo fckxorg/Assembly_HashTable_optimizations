aabb92574b21e0b72458d8886d579ec5107536a1
Kristina Kulabukhova <kulabukhova.ka@phystech.edu>
2020-04-26 22:13:02 +0300

    separated hashes

--- config.h
+++ config.h
@@ -1,3 +1,3 @@
 const int TableSize = 997;
 const int N_WORDS   = 24000;
-const int N_SAMPLES = 100000;
+const int N_SAMPLES = 1000000;
--- main.cpp
+++ main.cpp
@@ -2,10 +2,9 @@
 #include <algorithm>
 #include <random>
 #include <fstream>
-#include <cstring>
 #include <list>
 
-#include "config.h"
+#include "hashes.h"
 
 template <typename T>
 class Node
@@ -70,109 +69,6 @@ class List
 
         void erase(T value){}
 
-};
-
-
-class HashFunction
-{
-    public:
-        HashFunction() = default;
-        ~HashFunction() = default;
-
-        virtual int operator()(char* string)
-        {
-            return 0;
-        }
-};
-
-class ConstantHash : public HashFunction
-{
-    public:
-        ConstantHash() = default;
-        ~ConstantHash() = default;
-        int operator()(char* string)
-        {
-            return 0;
-        }
-};
-
-class StrlenHash : public HashFunction
-{
-    public:
-        StrlenHash() = default;
-        ~StrlenHash() = default;
-        int operator()(char* string)
-        {
-            return strlen(string);
-        }
-};
-
-class ASCIILenHash : public HashFunction
-{
-    public:
-        ASCIILenHash() = default;
-        ~ASCIILenHash() = default;
-        int operator()(char* string)
-        {
-            int sum = 0;
-            int len = strlen(string);
-            while(*string)
-            {
-                sum += *string;
-                ++string;
-            }
-            return sum / len;
-        }
-};
-
-class ASCIIXorRotate : public HashFunction
-{
-    public:
-        ASCIIXorRotate() = default;
-        ~ASCIIXorRotate() = default;
-        int operator()(char* string)
-        {
-            if(!*string)
-            {
-                return 0; 
-            }
-
-            uint32_t hash = 0;
-            while(*string)
-            {
-                hash = hash ^ (*string);
-                uint32_t oldest_bit = hash & 0x80000000;
-                oldest_bit >>= 31;    
-                hash <<= 1;
-                hash |= oldest_bit;
-                ++string;
-            }
-            return hash % TableSize;
-        }
-};
-
-class JenkinsHash : public HashFunction
-{
-    public:
-        JenkinsHash() = default;
-        ~JenkinsHash() = default;
-        int operator()(char* string)
-        {
-            int len = strlen(string);
-            size_t i = 0;
-            uint32_t hash = 0;
-
-            while (i != len) 
-            {
-                hash += string[i++];
-                hash += hash << 10;
-                hash ^= hash >> 6;
-            }
-            hash += hash << 3;
-            hash ^= hash >> 11;
-            hash += hash << 15;
-            return hash % TableSize;
-        }
 };
 
 template <typename T, typename Functor>
@@ -268,6 +164,7 @@ int main()
 
     std::ofstream out;
     out.open("test.out");
+    out << "Test" << std::endl;
     table.dump_lists_lens(out);
     out.close();
     in.close();

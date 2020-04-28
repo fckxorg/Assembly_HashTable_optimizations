b0d35f52a8d0bc68630fe26b5d2284b9ce1a86ed
Kristina Kulabukhova <kulabukhova.ka@phystech.edu>
2020-04-28 12:52:15 +0300

    taking the right stats

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
 const int TABLE_SIZE = 997;
 const int N_WORDS   = 24000;
-const int N_SAMPLES = 10000000;
+const int N_SAMPLES = 1000000;
 const int WORD_SIZE = 50;
--- hashes.h
+++ hashes.h
@@ -88,53 +88,20 @@ class JenkinsHash : public HashFunction
         ~JenkinsHash() = default;
         int operator()(char* string)
         {
+            uint32_t i = 0;
             uint32_t len = strlen(string);
             uint32_t hash = 0;
 
 
-            /*hile (i != len) 
+            while (i != len) 
             {
                 hash += string[i++];
-                hash *= 1024;//hash += hash << 10;
-                hash ^= hash / 64;//hash ^= hash >> 6;
-            }*/
-
-        asm("mov    eax, %1\n\t"       
-            "mov    ecx, %2\n\t"
-            "mov    rdi, %3\n\t"
-            "jenkins_loop:\n\t"
-            "cmp    rcx, 0\n\t"
-            "je     jenkins_loop_end\n\t"
-            "dec    ecx\n\t"
-            "xor    ebx, ebx\n\t"
-            "mov    bl, byte [rdi]\n\t"
-            "add    eax, ebx\n\t"
-            "mov    ebx, eax\n\t"
-            "shl    ebx, 10\n\t"
-            "add    eax, ebx\n\t"
-            "mov    ebx, eax\n\t"
-            "shr    ebx, 6\n\t"
-            "xor    eax, ebx\n\t"
-            "jmp    jenkins_loop\n\t"
-            "jenkins_loop_end:\n\t"
-            "mov    ebx, eax\n\t"
-            "shl    ebx, 3\n\t"
-            "add    eax, ebx\n\t"
-            "mov    ebx, eax\n\t"
-            "shr    ebx, 11\n\t"
-            "xor    eax, ebx\n\t"
-            "mov    ebx, eax\n\t"
-            "shl    ebx, 15\n\t"
-            "add    eax, ebx\n\t"
-            "mov    %0, eax\n\t"
-            :"=r"(hash)             
-            :"r"(hash), "r"(len), "r"(string)     
-            : "eax", "ebx", "ecx", "rdi"            
-            );
-
-            /*hash += hash * 8;//hash += hash << 3;
-            hash ^= hash / 2048;//hash ^= hash >> 11;
-            hash += hash * 32768; //hash += hash << 15;*/
+                hash += hash << 10;
+                hash ^= hash >> 6;
+            }
+            hash += hash << 3;
+            hash ^= hash >> 11;
+            hash += hash << 15;
             return hash % TABLE_SIZE;
         }
 };
